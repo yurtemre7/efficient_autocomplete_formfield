51b3e0161521899510d6e4d7c94003a8309aa0a2
Emre Yurtseven <emreyurtseven18@gmail.com>
2025-05-27 22:44:28 +0200

    Add new example project

--- example/lib/constants/data.dart
+++
@@ -1,8 +0,0 @@
-import 'package:example/models/person.dart';
-
-/// App title
-const title = 'efficient_autocomplete_formfield example';
-
-/// Data for the forms
-final people = <Person>[Person('Alice', '123 Main'), Person('Bob', '456 Main')];
-final letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
--- example/lib/models/person.dart
+++
@@ -1,9 +0,0 @@
-class Person {
-  final String name;
-  final String address;
-
-  Person(this.name, this.address);
-
-  @override
-  String toString() => name;
-}
